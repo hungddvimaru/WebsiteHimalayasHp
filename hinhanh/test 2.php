56f6a608f2d09b955504577df5b50967ff8100d2
HungLuongFamily <hungddvimaru@gmail.com>
2016-03-09 01:07:34 +0700

    Xóa các file không cần thiết và thư mục hình ảnh

--- hinhanh/test 2.php
+++
@@ -1,9 +0,0 @@
-    <?php
-/**
- * Created by PhpStorm.
- * User: HungLuongFamily
- * Date: 3/9/2016
- * Time: 1:01 AM
- */
-    $loaihang = \common\models\loaihang::find()->one();
-    return $loaihang;
--- hinhanh/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: HungLuongFamily
- * Date: 3/9/2016
- * Time: 1:00 AM
- */
